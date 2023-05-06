7d47b58729568c70d9243c11518b90bbd35aaa76
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-05-06 15:05:41 +0200

    prime prove del back end

---
+++ Exploro/src/app/classes/Geocacher.ts
@@ -0,0 +1,12 @@
+export class Geocacher{
+  id : number;
+  nome : string;
+  cognome : string;
+  username : string;
+  password : string;
+  mail : string;
+  cellulare : number;
+  dataDiNascita :Date;
+  livello : number;
+  puntiExp : number;
+}
--- Exploro/src/app/login/login.page.ts
+++ Exploro/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService) { }
 
   ngOnInit() {
+  }
+  onRegister(){
+    console.log(this.loginService.utentiList[0].mail);
+
   }
 
 }
