21c6a478c828e704d8946b240fc8935428a69b34
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-05-12 15:16:15 +0200

    sd

--- Exploro/src/app/component/map/map.component.ts
+++ Exploro/src/app/component/map/map.component.ts
@@ -3,6 +3,9 @@ import { ElementRef } from '@angular/core';
 import { Admin } from 'src/app/classes_&_services/Admin';
 import { CacheService } from 'src/app/classes_&_services/Cache.service';
 import { Cache } from 'src/app/classes_&_services/Cache';
+import { Geolocation } from '@capacitor/geolocation';
+
+
 
 
 @Component({
@@ -13,23 +16,94 @@ import { Cache } from 'src/app/classes_&_services/Cache';
 export class MapComponent implements OnInit, AfterViewInit {
   @ViewChild('map') mapElementRef: ElementRef;
 
+
+
   constructor(private reneder: Renderer2, private cacheSrv: CacheService) { }
+  currentLatitude : number;
+  currentLongitude : number;
 
   ngOnInit() { }
 
   ngAfterViewInit(): void {
-    this.getGoogleMaps().then(googleMaps => {
+
+    this.printCurrentPosition().then(el =>{
+      this.currentLatitude = el.coords.latitude;
+      this.currentLongitude = el.coords.longitude;
+      console.log("porco dio");
+      console.log(this.currentLatitude);
+      console.log("porca madonna");
+      console.log(this.currentLatitude);
+      console.log("mannaggia a cristo");
+
+      this.getGoogleMaps().then(googleMaps => {
+        const mapEl = this.mapElementRef.nativeElement;
+        const map = new googleMaps.Map(mapEl, {
+          //center: { lat: 42.349745, lng: 13.399413 },
+          center: {lat:this.currentLatitude , lng:this.currentLongitude },
+          zoom: 15
+        });
+        googleMaps.event.addListenerOnce(map, 'idle', () => {
+          this.reneder.addClass(mapEl, 'visible');
+        });
+        const features = this.buildFeatures(googleMaps);
+
+        for (let i = 0; i < features.length; i++) {
+          const marker = new googleMaps.Marker({
+            position: features[i].position,
+            icon: this.buildSVGMArker(googleMaps, features[i]),
+            title: "ciao",
+            map: map,
+          });
+
+
+          const contentString =
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<h1 style="color:black;" id="firstHeading" class="firstHeading">' + features[i].nome + "</h1>" +
+            '<div id="bodyContent">' +
+            "<p style='color:black;'>" + features[i].descr + "</p>" +
+            "<p style='color:black;'>" + features[i].lat + "," + features[i].long + "</p>" +
+            "</div>" +
+            "</div>";
+          const infowindow = new googleMaps.InfoWindow({
+            content: contentString,
+            ariaLabel: "Uluru",
+          });
+
+          //marker di prova per la posizione attuale
+
+          let markerPosition = new googleMaps.Marker({
+            position: new googleMaps.LatLng(this.currentLatitude, this.currentLongitude),
+            title: "position",
+            map: map,
+          });
+          //
+
+          marker.addListener("click", () => {
+            map.setZoom(18);
+            map.setCenter(marker.getPosition());
+            infowindow.open({
+              anchor: marker, map,
+            });
+          });
+        }
+
+      }).catch(err => {
+        console.log(err)
+      });
+
+    });
+    /*this.getGoogleMaps().then(googleMaps => {
       const mapEl = this.mapElementRef.nativeElement;
       const map = new googleMaps.Map(mapEl, {
         center: { lat: 42.349745, lng: 13.399413 },
+        //center: {lat:currentP.then(el => {el.coords.latitude}) , lng:currentP.then(el => {el.coords.longitude}) },
         zoom: 15
       });
-
       googleMaps.event.addListenerOnce(map, 'idle', () => {
         this.reneder.addClass(mapEl, 'visible');
       });
-
-
       const features = this.buildFeatures(googleMaps);
 
       for (let i = 0; i < features.length; i++) {
@@ -57,7 +131,8 @@ export class MapComponent implements OnInit, AfterViewInit {
         });
 
         //marker di prova per la posizione attuale
-        const markerPosition = new googleMaps.Marker({
+
+        let markerPosition = new googleMaps.Marker({
           position: new googleMaps.LatLng(42.349745, 13.399413),
           title: "position",
           map: map,
@@ -72,12 +147,15 @@ export class MapComponent implements OnInit, AfterViewInit {
           });
         });
       }
+       console.log("antanifine");
+       console.log(this.currentLatitude);
+       console.log("domanifine");
 
 
     }).catch(err => {
       console.log(err)
     });
-
+    */
   }
   private getGoogleMaps(): Promise<any> {
     const win = window as any;
@@ -132,5 +210,9 @@ export class MapComponent implements OnInit, AfterViewInit {
       scale: 15,
     }
     return svgMarker;
+  }
+  async printCurrentPosition() :Promise<any> {
+    const coordinates = await Geolocation.getCurrentPosition();
+    return Promise.resolve(coordinates);
   }
 }
