e52033c2e95553f49816ae2974a0e3e3876778f7
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-05-21 01:51:45 +0200

    sistema di rilascio feddback fatto

--- Exploro/src/app/pages/cache/dettagli-cache/dettagli-cache.page.ts
+++ Exploro/src/app/pages/cache/dettagli-cache/dettagli-cache.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Cache } from 'src/app/classes_&_services/Cache';
 import { CacheService } from 'src/app/classes_&_services/Cache.service';
+import { Feedback } from 'src/app/classes_&_services/Feedback';
+import { FeedbackService } from 'src/app/classes_&_services/FeedbackService';
 import { GeocacherService } from 'src/app/classes_&_services/Geocacher.service';
 import { SessioneService } from 'src/app/classes_&_services/Sessione.service';
 
@@ -19,18 +22,25 @@ export class DettagliCachePage implements OnInit {
   headerAlert: string
   messageAlert: string
   selezionato: boolean
+  feedback: Feedback = new Feedback
 
-  constructor(private router: Router, private cacheService: CacheService, private sessioneSrv: SessioneService, private utenteService: GeocacherService, private alertController: AlertController) { }
+  constructor(private router: Router,
+    private cacheService: CacheService,
+    private sessioneSrv: SessioneService,
+    private utenteService: GeocacherService,
+    private alertController: AlertController,
+    private feedbackSrv: FeedbackService) { }
 
   ngOnInit() {
     let idCache = parseInt(this.router.getCurrentNavigation()?.finalUrl?.queryParams['idCache'])
     this.cache = this.cacheService.findCacheById(idCache)
-
     if (this.utenteService.findGeocacherById(this.sessioneSrv.returnIdByJson(localStorage.getItem("geocacher"))).cacheTrovate.includes(this.cache.id)) {
       this.cacheTrovata = true
     } else {
       this.cacheTrovata = false
     }
+    let id_utente_sessione = this.sessioneSrv.returnIdByJson(localStorage.getItem("geocacher"))
+    this.feedback = this.feedbackSrv.getFeedbackByUtenteCache(id_utente_sessione, this.cache.id)
   }
 
   async VerificaParolaOrdine() {
@@ -72,11 +82,23 @@ export class DettagliCachePage implements OnInit {
     this.isAlertOpen = false
   }
 
-
   setCacheTrovata() {
     this.utenteService.addCacheTrovata(this.sessioneSrv.returnIdByJson(localStorage.getItem("geocacher")), this.cache.id)
     this.selezionato = true
     this.cacheTrovata = !this.cacheTrovata
+  }
+
+  InviaFeedback(form: NgForm) {
+    this.feedback.id_utente = this.sessioneSrv.returnIdByJson(localStorage.getItem("geocacher"))
+    this.feedback.id_cache = this.cache.id
+    this.feedbackSrv.aggiungiFeedback(this.feedback)
+    this.CheckFeedbackGiaLasciato()
+
+  }
+
+  CheckFeedbackGiaLasciato(): boolean {
+    let id_utente_sessione = this.sessioneSrv.returnIdByJson(localStorage.getItem("geocacher"))
+    return this.feedbackSrv.isFeedbackLasciato(id_utente_sessione, this.cache.id)
   }
 
 }
